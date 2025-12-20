af73cd53b6176f191bef88f4cb4d2dd704c426b6
Emilia <emi@myepicserver.net>
2025-12-20 14:01:27 -0500

    Update InstantDeath.java

--- src/main/java/my/epic/instantdeath/InstantDeath.java
+++ src/main/java/my/epic/instantdeath/InstantDeath.java
@@ -5,7 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import my.epic.bukkit.Metrics;
+import org.bstats.bukkit.Metrics;
 
 import java.util.HashMap;
 import java.util.Map;
